c6ed633c17ccebfe6fd82c3532e761e236fbe80b
vkojii <vkojiii@users.noreply.github.com>
2026-07-27 01:10:15 +0200

    Fish A Brainrot: adjust TP position forward

--- games/137372183607049.lua
+++ games/137372183607049.lua
@@ -22,7 +22,7 @@ return function(section, data)
 
     getgenv().farmActive = false
 
-    local fishZoneCF = CFrame.new(44.133, 5.129, -78)
+    local fishZoneCF = CFrame.new(44.133, 5.129, -90)
 
     local function tpTo(cf)
         pcall(function()
